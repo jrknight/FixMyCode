d9eeb72700a9d52b6c0689f6eea9ce6e0766acf5
Joshua Knight <jrknight@damonteranchhs.com>
2018-10-04 19:12:18 -0700

    Added additional DB functionality

---
+++ FixMyCode/FixMyCodeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FixMyCode.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FixMyCode
+{
+    public class FixMyCodeDbContext : DbContext
+    {
+
+        public FixMyCodeDbContext(DbContextOptions<FixMyCodeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Query> Queries { get; set; }
+
+    }
+}
