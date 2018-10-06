8294211e44619ba4395dd3e70f5051636a3aa369
Elijah Boucahrd <elijahboucharddrhs@gmail.com>
2018-10-06 12:01:32 -0700

    DBContextFactory

---
+++ FixMyCode/DesignTimeDBContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace FixMyCode
+{
+    public class DesignTimeDBContextFactory : IDesignTimeDbContextFactory<FixMyCodeDbContext>
+    {
+        public FixMyCodeDbContext CreateDbContext(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+
+            var Configuration = builder.Build();
+
+            //SQL Server location needs to change
+            //var options = new DbContextOptionsBuilder<FixMyCodeDbContext>().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LibraryDB;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
+            return new FixMyCodeDbContext(options);
+        }
+    }
+}
