b49bc6f20389daa45fe3540f7f1d02814bdf87ec
ColtonJacobson23 <coltonjjacobson@gmail.com>
2018-10-06 13:02:31 -0700

    Testing Comments.

--- FixMyCode/Startup.cs
+++ FixMyCode/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+/*Testing Git
+Done testing*/
 
 namespace FixMyCode
 {
