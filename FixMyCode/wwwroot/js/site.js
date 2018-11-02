1c443a69bc1581a08b0f7b53fb7c251c0440dc9b
Elijah Boucahrd <elijahboucharddrhs@gmail.com>
2018-11-02 10:50:46 -0700

    changes

--- FixMyCode/wwwroot/js/site.js
+++ FixMyCode/wwwroot/js/site.js
@@ -47,3 +47,22 @@ function d() {
         document.getElementById("confirmCheck").style.visibility = "hidden";
     }
 }
+
+$(document).delegate('#textbox', 'keydown', function (e) {
+    var keyCode = e.keyCode || e.which;
+
+    if (keyCode == 9) {
+        e.preventDefault();
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        // set textarea value to: text before caret + tab + text after caret
+        $(this).val($(this).val().substring(0, start)
+            + "\t"
+            + $(this).val().substring(end));
+
+        // put caret at right position again
+        this.selectionStart =
+            this.selectionEnd = start + 1;
+    }
+});
